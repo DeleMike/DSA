94540c4d6e8e16e5cb3b5c3836c127ffc132387c
Akindele Michael <akindelemichael65@gmail.com>
2024-01-04 16:03:42 +0100

    add abstract class definitions

---
+++ dsa_practice/abstract_class.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+class Polygon(ABC):
+    @abstractmethod
+    def noofsides(self): 
+        pass
+    
+class Triangle(Polygon):
+    def noofsides(self): 
+        print("I have 3 sides") 
+  
+  
+class Pentagon(Polygon): 
+    # overriding abstract method 
+    def noofsides(self): 
+        print("I have 5 sides") 
+  
+  
+class Hexagon(Polygon): 
+  
+    # overriding abstract method 
+    def noofsides(self): 
+        print("I have 6 sides")    
+        
+# Driver code 
+R = Triangle() 
+R.noofsides() 
+  
+R = Pentagon() 
+R.noofsides() 
+  
+K = Hexagon() 
+K.noofsides() 
