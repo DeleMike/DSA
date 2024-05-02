87a17b16d402ed248baab4bdcd0bb8aba127e5e8
Akindele Michael <akindelemichael65@gmail.com>
2024-05-02 18:23:17 +0100

    add remove element

---
+++ dsa_practice/remove_element.py
@@ -0,0 +1,47 @@
+"""EASY
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+"""
+def removeElement(nums, val):
+    # Initialize two pointers
+    left, right = 0, len(nums)
+
+    while left < right:
+        # If the current element is equal to val, replace it with the element at the right pointer
+        if nums[left] == val:
+            nums[left] = nums[right - 1]
+            right -= 1
+        else:
+            left += 1
+
+    # The array nums has been modified in-place up to the right pointer
+    return left
+            
+            
+def removeVal(nums,val, index):
+    for i in range(index, len(nums)):
+        nums[i] = nums[i+1]
+    print(nums)
+    
+
+print(removeElement([3,2,2,3],3))
+# print(removeElement([0,1,2,2,3,0,4,2],3))
